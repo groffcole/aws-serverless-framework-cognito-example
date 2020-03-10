483c7428dcd0bbb0c2b4df6e0cdedf59ede19cad
Cole Groff <groffcole@gmail.com>
2020-03-09 21:58:29 -0500

    getting them cognitos in order

--- handler.js
+++ handler.js
@@ -1,6 +1,32 @@
-module.exports.hello = async event => {
+module.exports.someAdminFunction = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({message: "hello, world."})
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true
+    },
+    body: JSON.stringify({ message: "hello, admin." })
+  };
+};
+
+module.exports.someRegularUserFunction = async (event) => {
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true
+    },
+    body: JSON.stringify({ message: "hello, regular user." })
+  };
+};
+
+module.exports.hello = async (event) => {
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true
+    },
+    body: JSON.stringify({ message: "hello, world." })
   };
 };
